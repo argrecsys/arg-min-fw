c719a4fa9aa1aa7c9f0283ac2fb3c1333fa00554
Andres Segura-Tinoco <seguraandres7@gmail.com>
2024-04-23 05:55:00 -0500

    Update Framework
    The functions load_data and split_data have been completed.

--- src/app/framework/engine.py
+++ src/app/framework/engine.py
@@ -5,16 +5,33 @@ from sklearn.utils import shuffle
 
 class Engine:
 
-    def __init__(self):
-        pass
+    def __init__(self, verbose: bool = False):
+        self.verbose = verbose
 
-    def load_dataset(self, dataset_path: str):
+    def __print(self, values):
+        if self.verbose:
+            print(values)
+
+    def load_data(
+        self, dataset_path: str, text_column: str, label_column: str, label_dict: dict
+    ):
         self.dataset = pd.read_csv(dataset_path)
-        print(self.dataset)
+        self.dataset = self.dataset[[text_column, label_column]]
+        self.dataset = self.dataset.rename(
+            columns={text_column: "sentence", label_column: "label"}
+        )
+        self.dataset = self.dataset.replace({"label": label_dict})
+        self.__print(self.dataset)
 
     def split_data(
-        df, target_column, test_size=0.2, validation_size=0.1, random_state=None
+        self,
+        test_size: float = 0.2,
+        validation_size: float = 0.1,
+        shuffled: bool = False,
+        random_state: int = None,
     ):
+        df = self.dataset
+        target_column = "label"
 
         # Split the data into a temporary train set and test/validation set
         train, temp = train_test_split(
@@ -32,4 +49,11 @@ class Engine:
             random_state=random_state,
         )
 
-        return train, validation, test
+        if shuffled:
+            self.training_set = shuffle(train)
+            self.validation_set = shuffle(validation)
+            self.test_set = shuffle(test)
+        else:
+            self.training_set = train
+            self.validation_set = validation
+            self.test_set = test
